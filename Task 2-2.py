ce21d1c889cea451c8872b8cce06a6eb9cfb0416
RomanMolodtsov <roman.stikhiya@gmail.com>
2023-09-24 12:17:35 +0300

    Домашнее задание №2. Задание 2 исправлено.

--- Task 2-2.py
+++ Task 2-2.py
@@ -1,7 +1,13 @@
-lst = [234, 2, 54, 645, 23, 75, 1, 75, 43, 8789, 5]
-a = lst[0]
+# lst = [234, 2, 54, 645, 23, 75, 1, 75, 43, 8789, 5]
+# a = lst[0]
+# for i in lst:
+#     for j in range(0, len(lst)-1):
+#         if lst[j] < a:
+#             a = int(lst[j])
+# print(a)
+
+lst=[234, 2, 54, 645, 23, 75, 1, -75, 43, 8789, 5]
+а = lst[0]
 for i in lst:
-    for j in range(0, len(lst)-1):
-        if lst[j] < a:
-            a = int(lst[j])
-print(a)
+    if i <а: а = i
+print(а)
