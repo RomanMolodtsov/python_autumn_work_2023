c864a56ce1260d65cda980a5a6867f30a3855806
RomanMolodtsov <roman.stikhiya@gmail.com>
2023-09-28 22:47:21 +0300

    Исправлена.

--- Task 4-3.py
+++ Task 4-3.py
@@ -1,10 +1,13 @@
-str1 = input('Введите первое предложение: ').lower()
-str2 = input('Введите второе предложение: ').lower()
+s1 = input()
+s2 = input()
+ab ='qwertyuiopasdfghjklzxcvbnm'
+ds1 = {}
+ds2 = {}
+for i in s1.lower():
+    if i in ab:
+        ds1[i] = ds1.get(i, 0) + 1
+for i in s2.lower():
+    if i in ab:
+        ds1[i] = ds2.get(i, 0) + 1
+print(ds1 == ds2)
 
-s1 = list(str1)
-s2 = list(str2)
-
-s1.sort()
-s2.sort()
-
-print(s1 == s2)
