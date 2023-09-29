94632e4b24b5e9c4185786b48cb9ddf442731035
RomanMolodtsov <roman.stikhiya@gmail.com>
2023-09-29 19:48:21 +0300

    Испр.

--- Task 5-1.py
+++ Task 5-1.py
@@ -1,3 +1,13 @@
-n = int(input("Введите число строк: "))
-for i in range(n):
-    print(' '.join(map(str, str(11 ** i))))
+n = int(input())
+lst = [0, 1, 0]
+print(1)
+for i in range(2, n+1):
+    x = lst[0]
+    for j in range(1, i+1):
+        y = x + lst[j]
+        print(y, end = ' ')
+        x = lst[j]
+        lst[j] = y
+    print()
+    lst.append(0)
+
